0e07cb3537716a93aa9024a47f335dbb8542a116
Jose Ignacio Bello gutierrez <jibello@hotmail.com>
2020-11-12 20:23:02 +0100

    Updates

---
+++ precache-manifest.5ee57bece37e03727235007c304102a3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3682d8464252cbaf76f817e088dd94a",
+    "url": "/twittorfrontend/index.html"
+  },
+  {
+    "revision": "7989eef243931e452cd0",
+    "url": "/twittorfrontend/static/css/2.e514ba11.chunk.css"
+  },
+  {
+    "revision": "a0228841482915c58075",
+    "url": "/twittorfrontend/static/css/main.325ebe78.chunk.css"
+  },
+  {
+    "revision": "7989eef243931e452cd0",
+    "url": "/twittorfrontend/static/js/2.8ffbfcd9.chunk.js"
+  },
+  {
+    "revision": "7e9e628f0cd67aad9b800147a28c9587",
+    "url": "/twittorfrontend/static/js/2.8ffbfcd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0228841482915c58075",
+    "url": "/twittorfrontend/static/js/main.af303904.chunk.js"
+  },
+  {
+    "revision": "e1bc0be0f6a6b62bef09",
+    "url": "/twittorfrontend/static/js/runtime-main.7a9c68df.js"
+  },
+  {
+    "revision": "29a28302efe4f7f1179bee2a29f768c9",
+    "url": "/twittorfrontend/static/media/avatar-no-found.29a28302.png"
+  },
+  {
+    "revision": "e2eae07daffddb886cf6729f4e1fe5a2",
+    "url": "/twittorfrontend/static/media/camera.e2eae07d.svg"
+  },
+  {
+    "revision": "9ad3d3794c5c49df0b0f113de09f828e",
+    "url": "/twittorfrontend/static/media/close.9ad3d379.svg"
+  },
+  {
+    "revision": "d97ec0025cb3a34f8643ecfc1ea5fcfe",
+    "url": "/twittorfrontend/static/media/date-birth.d97ec002.svg"
+  },
+  {
+    "revision": "24d5c66ecd63397ead19f5ded66fb603",
+    "url": "/twittorfrontend/static/media/error-404.24d5c66e.png"
+  },
+  {
+    "revision": "e4159b7740831a15433a0341db35ce39",
+    "url": "/twittorfrontend/static/media/link.e4159b77.svg"
+  },
+  {
+    "revision": "d8450c0bad86ec78f812266e8af81e68",
+    "url": "/twittorfrontend/static/media/location.d8450c0b.svg"
+  },
+  {
+    "revision": "073ae2614f7fe7164eb9302cc47a65f9",
+    "url": "/twittorfrontend/static/media/logo-blue.073ae261.png"
+  },
+  {
+    "revision": "7fe61d83a76a981ee9779185354b6e21",
+    "url": "/twittorfrontend/static/media/logo-white.7fe61d83.png"
+  }
+]);
